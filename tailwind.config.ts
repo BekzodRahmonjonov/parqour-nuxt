c59af7d37bdb23763a291391c34afb7f3df61856
Muhammadjon Polvanov <m.polvanov@uic.group>
2023-06-07 15:57:05 +0500

    style: tailwind color palettes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,19 +15,21 @@ export default <Partial<Config>>{
   ],
   theme: {
     extend: {
-      blue: {
-        100: '#A2BCDE',
-        200: '#52618F',
-        700: '#191F2E',
-      },
-      white: {
-        DEFAULT: '#ffffff',
-      },
-      gray: {
-        DEFAULT: '#B8BABE',
-        100: '#D1D2D5',
-        200: '#919299',
-        300: '#E5E7EE',
+      colors: {
+        blue: {
+          100: '#A2BCDE',
+          200: '#52618F',
+          700: '#191F2E',
+        },
+        white: {
+          DEFAULT: '#ffffff',
+        },
+        gray: {
+          DEFAULT: '#B8BABE',
+          100: '#D1D2D5',
+          200: '#919299',
+          300: '#E5E7EE',
+        },
       },
     },
   },
