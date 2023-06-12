7a0d14be22e05f19e706bd04ab98b4614f708a90
Muhammadjon Polvanov <m.polvanov@uic.group>
2023-06-12 18:29:05 +0500

    feat: window scroll to top

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,43 @@
 <template>
   <div>
-    <PushMessage />
-    <div class="dark:bg-blue-700 transition-200">
-      <LayoutHeader />
-      <div class="pt-[104px] lg:pt-[235.9px]">
-        <slot />
+    <div>
+      <PushMessage />
+
+      <div class="dark:bg-blue-700 transition-200">
+        <LayoutHeader ref="header" />
+        <div class="pt-[104px] lg:pt-[235.9px]">
+          <slot />
+        </div>
+        <LayoutFooter />
       </div>
-      <LayoutFooter />
+      <button
+        v-if="windowIsScrolled"
+        class="fixed right-8 bottom-8 flex-y-center justify-center text-2xl text-white icon-arrow-left rotate-90 w-12 h-12 transition-200 hover:bg-blue-600 rounded-full bg-blue-100 z-20"
+        @click="scrollToTop"
+      />
     </div>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useWindowScroll } from '@vueuse/core'
+
+const scroll = useWindowScroll()
+const scrollTop = scroll.y
+const windowIsScrolled = ref(false)
+const header = ref()
+function scrollToTop() {
+  window.scrollTo(0, 0)
+}
+
+watch(
+  () => scrollTop,
+  (value) => {
+    if (value.value > 150) {
+      windowIsScrolled.value = true
+    } else {
+      windowIsScrolled.value = false
+    }
+  },
+  { deep: true }
+)
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,20 @@
     <SectionsLatestNews />
     <SectionsMainNews />
     <SectionsNews class="py-6 lg:py-10" />
-    <CommonAdBanner image="/images/ad/ad.png" />
+    <CommonAdBanner image="/images/advertising/adver.png" />
     <SectionsAuthor class="py-6 lg:py-10" />
     <SectionsReports :data="reportsData" />
-    <CommonAdBanner image="/images/ad/yellow.png" class="pt-6 lg:pt-10" />
+    <CommonAdBanner
+      image="/images/advertising/yellow.png"
+      class="pt-6 lg:pt-10"
+    />
     <SectionsPhotoReports class="pt-6 lg:pt-10" />
     <SectionsPodcasts class="py-6 lg:py-10" />
     <SectionsInterviews class="pb-6 lg:pb-10" />
-    <CommonAdBanner image="/images/ad/airways.png" class="pb-6 lg:pb-10" />
+    <CommonAdBanner
+      image="/images/advertising/airways.png"
+      class="pb-6 lg:pb-10"
+    />
     <SectionsColumns class="pb-6 lg:pb-10" />
     <SectionsSocial />
     <SectionsAnalysis class="py-6 lg:py-10" />
