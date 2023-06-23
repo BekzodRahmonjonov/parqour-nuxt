9fc1adb9efb278ecb7a1455ebc1581013c1d2cb2
Azizbek Abduqahhorov <a.abduqahhorov@uic.group>
2023-06-23 18:52:59 +0500

    feat: add contact modal

---
+++ pages/fake-page.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <ModalContactModal :show="contactModal" @close="contactModal = false" />
+
+    <CommonButton :text="'openModal'" @click="contactModal = true" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const contactModal = ref(false)
+</script>
