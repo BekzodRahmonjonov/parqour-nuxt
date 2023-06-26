f0266ff9a0a23146a59ff8d7e857aa22d9e9afa0
Ilhom <ilhom@MacBook-Air-Ilhom.local>
2023-06-27 00:59:50 +0500

    feat: advertisimnet sidebar section

---
+++ components/Temp/AdvetisimentBanner.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="float-right grid gap-6">
+    <img
+      v-for="(item, idx) in items"
+      :key="idx"
+      class="w-[274px]"
+      :src="item.img"
+      alt="advert"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+const items = [
+  {
+    id: '0',
+    img: '/temp/advert-img1.png',
+  },
+  {
+    id: '1',
+    img: '/temp/advert-img2.png',
+  },
+]
+</script>
+
+<style></style>
