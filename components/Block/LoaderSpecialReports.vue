6bdc13c6be1281a6659bcb60a689469ed4078e27
Javohir Berdiyev <j.berdiyev@uic.group>
2023-06-27 11:41:17 +0500

    feat: SpecialReportsSingle card added

---
+++ components/Block/LoaderSpecialReports.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="p-3 !pl-[202px] flex flex-col gap-2 h-[180px] sm:p-5 bg-[#F5F5F8] w-full md:w-full rounded-lg text-dark relative transition-200 group-hover:-translate-y-1"
+  >
+    <div class="!absolute absolute-y-center -left-5 rounded-lg">
+      <BlockPreloader :loading="true" width="202px" height="140px" />
+    </div>
+
+    <BlockPreloader :loading="true" width="100px" height="16px" />
+    <BlockPreloader :loading="true" width="600px" height="24px" />
+    <BlockPreloader :loading="true" width="240px" height="24px" />
+    <BlockPreloader :loading="true" width="100px" height="16px" />
+    <BlockPreloader :loading="true" width="100px" height="16px" />
+  </div>
+</template>
--- pages/special-reports/index.vue
+++ pages/special-reports/index.vue
@@ -1,4 +1,26 @@
 <template>
-  <div>Specific Reports Page</div>
+  <div class="container">
+    <CommonPageWrapper
+      :title="$t('special_reports')"
+      :text="$t('column_text')"
+      class="mt-8"
+    >
+      <div class="grid grid-cols-1 gap-8">
+        <CardsSpecialReportsSingle
+          v-for="(item, index) in specialReports"
+          :key="index"
+          :data="item"
+          class="min-h-[180px]"
+        />
+
+        <BlockLoaderSpecialReports />
+      </div>
+      <template #aside>
+        <img src="https://picsum.photos/200/400" class="w-full" alt="absolute-y-center -left-5 rounded-lg" />
+      </template>
+    </CommonPageWrapper>
+  </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { specialReports } from '~/data'
+</script>
