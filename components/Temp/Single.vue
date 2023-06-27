02331fafd9f41efdf6fa36545ad665950e15680f
Ilhom <ilhom@MacBook-Air-Ilhom.local>
2023-06-27 10:01:30 +0500

    faet: single section

---
+++ components/Temp/Single.vue
@@ -0,0 +1,273 @@
+<template>
+  <div
+    class="vhtml-text mx-auto max-w-[785px] md:mt-16 mt-4 whitespace-pre-line"
+  >
+    <h3>
+      «Это абсолютно необоснованные слова» — Шавкат Мирзиёев о введении
+      подоходного налога мигрантов
+      <i class="icon-unread check"></i>
+    </h3>
+    <div class="results flex-y-center gap-4 py-4">
+      <div class="flex-y-center gap-1">
+        <i class="icon-calendar-dotted"></i>
+        23 января, 21:53
+      </div>
+      <div class="flex-y-center gap-1">
+        <i class="icon-eye"></i>
+        224
+      </div>
+    </div>
+    <h4>
+      Кандидат в президенты Шавкат Мирзиёев отреагировал на предложение Алишера
+      Кадырова о том, что мигранты, работающие за рубежом, должны платить
+      налоги.
+    </h4>
+
+    <img src="@/public/temp/singleImg.png" alt="image" />
+    <p class="main__img-author">© Фото: Джасурбек Нарзуллаев</p>
+    <p>
+      «Наши граждане, которые уехали за границу на заработки, ищут возможности.
+      Тот, кто ищет возможности, бескорыстен. В социальных сетях обсуждают
+      предложение о введении налога на доходы для соотечественников, трудящихся
+      за рубежом. Это абсолютно необоснованные слова. Этого никогда не будет,
+      если я, дай Бог, продолжу свою деятельность в качестве главы государства.
+      Эти люди создают возможности для себя и своих родных, кормят свои семьи. Я
+      считаю таких людей своими детьми», — заявил Шавкат Мирзиёев в ходе встречи
+      с избирателями в Ташкентской области.
+    </p>
+
+    <blockquote>
+      «Наши граждане, которые уехали за границу на заработки, ищут возможности.
+      Тот, кто ищет возможности, бескорыстен. В социальных сетях обсуждают
+      предложение о введении налога на доходы для соотечественников, трудящихся
+      за рубежом.”
+      <br />
+      <br />
+      Шавкат Мирзиёев
+    </blockquote>
+    <p>
+      По словам президента, с каждым годом в Узбекистане предпринимаются меры по
+      облегчению положения мигрантов.
+    </p>
+    <br />
+    <p>
+      «Мы думаем о том, чтобы максимально снизить налоги, а не вводить новые.
+      Вчера на саммите СНГ я также поднял вопрос о мигрантах и внес новые
+      предложения по обеспечению их безопасности. Теперь у нас просят присылать
+      работников. Я призываю другие государства обучать наших соотечественников
+      и давать им достойную работу», — подчеркнул президент.
+    </p>
+    <br />
+
+    <p>
+      <a href="">Напомним</a>, ранее кандидат в президенты от партии «Миллий
+      Тикланиш» Алишер Кадыров предложил ввести обязательный налог на доходы для
+      узбекистанцев, работающих за рубежом. По его словам, деньги, которые
+      зарабатывают мигранты и присылают на родину, должны идти не только на
+      обеспечение их семей, но и на развитие страны.
+    </p>
+    <div class="banner">
+      <img src="@/public/temp/singleImg.png" alt="image" />
+      <p>
+        Узбекистан и Кыргызстан подписали пакет соглашений на сумму более $1,6
+        млрд — список
+      </p>
+      <a href="">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M13 11L22 2M22 2H16.6562M22 2V7.34375"
+            stroke="#A2BCDE"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2"
+            stroke="#A2BCDE"
+            stroke-width="1.5"
+            stroke-linecap="round"
+          />
+        </svg>
+      </a>
+    </div>
+    <p>
+      По словам президента, с каждым годом в Узбекистане предпринимаются меры по
+      облегчению положения мигрантов.
+    </p>
+    <br />
+    <p>
+      «Мы думаем о том, чтобы максимально снизить налоги, а не вводить новые.
+      Вчера на саммите СНГ я также поднял вопрос о мигрантах и внес новые
+      предложения по обеспечению их безопасности. Теперь у нас просят присылать
+      работников. Я призываю другие государства обучать наших соотечественников
+      и давать им достойн ую работу», — подчеркнул президент.
+    </p>
+    <ul>
+      <li>просмотр резюме;</li>
+      <li>собеседование с HR-специалистами;</li>
+      <li>знакомство с ключевыми сотрудниками;</li>
+    </ul>
+    <p>
+      «Мы думаем о том, чтобы максимально снизить налоги, а не вводить новые.
+      Вчера на саммите СНГ я также поднял вопрос о мигрантах и внес новые
+      предложения по обеспечению их безопасности. Теперь у нас просят присылать
+      работников. Я призываю другие государства обучать наших соотечественников
+      и давать им достойн ую работу», — подчеркнул президент.
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+<style scoped>
+.vhtml-text h3 {
+  color: #191f2e;
+  font-size: 32px;
+  font-weight: 700;
+  line-height: 136%;
+}
+
+.vhtml-text h4 {
+  color: #191f2e;
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 140%;
+  margin-bottom: 24px;
+}
+.main__img-author {
+  color: #919299 !important;
+  font-size: 12px !important;
+  font-style: italic;
+  line-height: 140% !important;
+  margin-bottom: 24px !important;
+}
+.vhtml-text p {
+  color: #2c3752;
+  font-size: 18px;
+  line-height: 150%;
+}
+.vhtml-text a {
+  color: #189fff;
+}
+.vhtml-text a:hover {
+  text-decoration: underline;
+}
+.vhtml-text img {
+  width: 100%;
+  height: auto;
+}
+
+.vhtml-text blockquote {
+  margin: 24px 0;
+  padding: 24px;
+  position: relative;
+  background: #f8fafc;
+  border-radius: 4px;
+}
+.vhtml-text blockquote p {
+  margin-top: 0;
+  padding-top: 0;
+  padding-bottom: 8px;
+}
+.vhtml-text blockquote p,
+.vhtml-text blockquote {
+  color: #2c3752;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 150%;
+}
+.vhtml-text blockquote:after {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  height: 100%;
+  width: 4px;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+  background: #52618f;
+}
+.vhtml-text blockquote:before {
+  width: 240px;
+  height: 240px;
+  border-radius: 150px;
+  position: absolute;
+  left: 12px;
+  top: 12px;
+}
+.banner {
+  border-radius: 4px;
+  background: #f8fafc;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 16px;
+  display: flex;
+  padding: 24px;
+  margin: 24px 0;
+}
+.banner img {
+  width: 96px;
+  height: 96px;
+  border-radius: 4px;
+  margin-right: 4px;
+}
+.banner p {
+  transform: translateY(50%);
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 136%;
+}
+.vhtml-text ol li,
+.vhtml-text ul li {
+  color: #2c3752;
+  font-size: 18px;
+  line-height: 150%;
+}
+.vhtml-text ul,
+.vhtml-text ol {
+  padding-left: 20px;
+  margin: 20px 0;
+}
+.vhtml-text ul {
+  list-style: disc;
+}
+.vhtml-text ol {
+  list-style: disc;
+}
+
+@media screen and (max-width: 768px) {
+  .vhtml-text p,
+  .vhtml-text blockquote {
+    font-size: 16px !important;
+    line-height: 140% !important;
+  }
+  .vhtml-text img,
+  .vhtml-text blockquote,
+  .vhtml-text ul,
+  .vhtml-text ol {
+    margin: 12px 0;
+  }
+  .vhtml-text blockquote {
+    padding: 10px 14px 10px 16px;
+  }
+  .vhtml-text table thead tr th {
+    @apply py-3 px-4 text-base;
+  }
+  .vhtml-text table tbody tr td {
+    @apply py-4 px-4 text-base;
+  }
+}
+@media (max-width: 640px) {
+  .vhtml-text ol li,
+  .vhtml-text blockquote,
+  .vhtml-text ul li {
+    font-size: 14px !important;
+  }
+}
+</style>
