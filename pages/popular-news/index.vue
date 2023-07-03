5530add6b71ffa5b2ec3d1965075013b9170479a
Abdukhamid <Abdukhamid000@gmail.com>
2023-07-03 11:38:37 +0500

    feat: popular

--- pages/popular-news/index.vue
+++ pages/popular-news/index.vue
@@ -1,24 +1,27 @@
 <template>
   <div class="container">
     <CommonPageWrapper class="mt-8">
+      <button
+        class="text-sm font-medium leading-126 text-white p-2 rounded-[30px] bg-blue-200"
+      >
+        Все
+      </button>
       <CommonDropdown class="bg-[#F5F6F9] py-[10px] px-3 rounded-md">
         <template #head>
           <div
             class="flex items-center gap-[23px] font-medium leading-130 text-blue-700"
           >
-            За неделью <span class="icon-chevron-right rotate-90 text-5"></span>
+            {{ sort[activeSortI] }}
+            <span class="icon-chevron-right rotate-90 text-5"></span>
           </div>
         </template>
-
         <li
-          class="p-3 hover:bg-[#F5F6F9] rounded-md font-medium leading-130 text-blue-700"
+          v-for="(item, i) in sort"
+          :key="i"
+          @click="onClick(i)"
+          class="p-3 hover:bg-[#F5F6F9] rounded-md font-medium leading-130 text-blue-700 transition-300"
         >
-          За неделью
-        </li>
-        <li
-          class="p-3 hover:bg-[#F5F6F9] rounded-md font-medium leading-130 text-blue-700"
-        >
-          За все время
+          {{ item }}
         </li>
       </CommonDropdown>
 
@@ -38,4 +41,11 @@
 </template>
 <script setup lang="ts">
 import { specialReports } from '~/data'
+
+const sort = ref(['За неделью', 'За все время'])
+const activeSortI = ref(0)
+
+const onClick = (index: number) => {
+  activeSortI.value = index
+}
 </script>
