0cdd1816a87d7dfecd39cec1a6a181703854dbc0
Ilhom <i.abdiroziqov@uic.group>
2023-07-07 15:11:16 +0500

    fix: reports section

--- pages/photo-reports/[slug].vue
+++ pages/photo-reports/[slug].vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    <!--    <CommonBreadcrumb :menu="breadcrumbLinks" />-->
+    <CommonBreadcrumb :menu="breadcrumbLinks" />
     <div class="container mt-8">
       <PhotoReportsSingleWrapper :single="photoData">
         <template #aside>
           <TempAdvetisimentBanner />
         </template>
+        <div class="mt-20 flex">
+          <NewsTags v-bind="{ tags: interviewTags }" />
+          <CommonLikeDislike />
+        </div>
       </PhotoReportsSingleWrapper>
     </div>
   </div>
@@ -13,14 +17,14 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 
-import PhotoReportSlider from '~/components/Slider/PhotoReportSlider.vue'
+import { interviewTags } from '~/data'
 import { photoData } from '~/data/fakeData'
 
 const { t } = useI18n()
-// const breadcrumbLinks = computed(() => [
-//   { title: 'Разборы', link: '/photo-reports' },
-//   { title: singleData.title, link: '/photo-reports' },
-// ])
+const breadcrumbLinks = computed(() => [
+  { title: 'Разборы', link: '/photo-reports' },
+  { title: photoData.title, link: '/photo-reports' },
+])
 
 const images = [
   {
